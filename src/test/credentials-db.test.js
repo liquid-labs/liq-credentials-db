532abd475bd8066eda666851af35df9bebba2a07
Zane Rockenbaugh <zane@liquid-labs.com>
2023-10-23 11:52:59 -0500

    additional testing

--- src/test/credentials-db.test.js
+++ src/test/credentials-db.test.js
@@ -1,12 +1,15 @@
-/* global describe expect test */
-import { CREDS_DB_CACHE_KEY } from '../constants'
+/* global beforeAll describe expect test */
+import { existsSync } from 'node:fs'
+import * as fsPath from 'node:path'
+
+import { CREDS_DB_CACHE_KEY, status, types } from '../constants'
 import { CredentialsDB } from '../credentials-db'
 
-const nullCache = {
+/*const nullCache = {
   creds : {},
   get   : (key) => { return nullCache.creds[key] },
   put   : (key, value) => { nullCache[key] = value }
-}
+}*/
 
 describe('CredentialsDB', () => {
   describe('constructor', () => {
@@ -18,6 +21,34 @@ describe('CredentialsDB', () => {
 
       new CredentialsDB({ cache }) // eslint-disable-line no-new
       expect(getWasCalledWith).toBe(CREDS_DB_CACHE_KEY)
+    })
+  })
+
+  describe('import', () => {
+    describe('by default', () => {
+      const dbPath = fsPath.join(__dirname, 'data', 'test-db.yaml')
+      let credDB
+
+      beforeAll(async () => {
+        const authTokenPath = fsPath.join(__dirname, 'data', 'bogus-github-api-token.yaml')
+        process.env.LIQ_CREDENTIALS_DB_PATH = dbPath
+
+        credDB = new CredentialsDB()
+        credDB.registerCredentialType({
+          key: 'BOGUS_API',
+          name: 'A test credential',
+          type: types.AUTH_TOKEN,
+          verifyFunc: () => true
+        })
+        await credDB.import({ key: 'BOGUS_API', srcPath: authTokenPath })
+      })
+
+      test('imports the credential file in-place', async () => expect(existsSync(dbPath)).toBe(true))
+
+      test('performs verification', () => {
+        const detail = credDB.detail('BOGUS_API')
+        expect(detail.status).toBe(status.SET_AND_VERIFIED)
+      })
     })
   })
 })
